5d0535541de1523421c9251431e2dd56f9020971
Isaac Rios <jirb1213@live.com.mx>
2025-05-29 17:11:12 -0600

    testing the modal in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import Modal from "./components/Modal/Modal";
 
 function App() {
   return (
     <>
       <Button label="click" variant="link" href="https://www.google.com" />
+      <Modal
+        title="header"
+        buttons={{ variant: "multiple", actions: { onPrimary: () => {} } }}
+      >
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
+        mollitia velit culpa? Dolor aspernatur laboriosam error ad recusandae
+        sunt repellat. A, sunt ad officiis fugiat quia expedita officia id quas?
+      </Modal>
     </>
   );
 }
