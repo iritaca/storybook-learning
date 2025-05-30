8a7dc55d92cb695f6a415f49cbe4acf65dfee488
Isaac Rios <jirb1213@live.com.mx>
2025-05-30 14:17:37 -0600

    global storybook config enhanced

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,14 +6,19 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
+    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
     "@chromatic-com/storybook",
     "@storybook/experimental-addon-test"
   ],
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
+  },
+  docs:{
+    autodocs:true, // Automatically generates docs pages from your component metadata (like props,types, and comments)
   }
 };
 export default config;
